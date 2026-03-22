dbad9326414498776c5f37e9b79c68c35bfec0e9
JungCollin <collinjung@naver.com>
2026-03-22 12:46:26 +0900

    feat: add relay stage dusk-subway-echo-weaving-inverse (#98)
    * feat: add relay stage dusk-subway-echo-weaving-inverse
    
    * feat: add relay stage dusk-subway-echo-weaving-inverse

--- community-stages/registry.js
+++ community-stages/registry.js
@@ -198,6 +198,15 @@ window.COMMUNITY_STAGE_REGISTRY = [
     path: "./memory-dodge/index.html",
   },
   {
+    id: "dusk-subway-echo-weaving-inverse",
+    title: "Dusk Subway: Echo Loom",
+    creator: { name: "jungcollin", avatar: null, github: "jungcollin" },
+    genre: "Echo puzzler / 역전 버스트",
+    clearCondition: "역전 구간을 모두 견디고 모든 코어 문양을 연결하면 클리어",
+    thumbnail: "./dusk-subway-echo-weaving-inverse/thumbnail.png",
+    path: "./dusk-subway-echo-weaving-inverse/index.html",
+  },
+  {
     id: "moon-greenhouse-orbit-herding-collapse",
     title: "Moon Greenhouse: Orbit Shepherd",
     creator: { name: "jungcollin", avatar: null, github: "jungcollin" },
