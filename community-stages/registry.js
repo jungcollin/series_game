7bbd920795bf3bac03dbe3ad19192015c7ff9969
jungcollin <collinjung@naver.com>
2026-03-22 12:44:51 +0900

    feat: add relay stage neon-observatory-orbit-herding-combo

--- community-stages/registry.js
+++ community-stages/registry.js
@@ -196,5 +196,14 @@ window.COMMUNITY_STAGE_REGISTRY = [
     clearCondition: "20초 동안 안전 구역을 기억하고 생존하기",
     thumbnail: "./memory-dodge/thumbnail.png",
     path: "./memory-dodge/index.html",
+  },
+  {
+    id: "neon-observatory-orbit-herding-combo",
+    title: "Neon Observatory: Orbit Shepherd",
+    creator: { name: "jungcollin", avatar: null, github: "jungcollin" },
+    genre: "Orbital survival / 콤보 임계치",
+    clearCondition: "콤보를 유지하며 위성 3기를 목표 링에 순서대로 통과시키면 클리어",
+    thumbnail: "./neon-observatory-orbit-herding-combo/thumbnail.png",
+    path: "./neon-observatory-orbit-herding-combo/index.html",
   }
 ];
